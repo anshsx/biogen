70e0940d19a8d844f7a73baa15bcdca66c349703
anshsx <erasearch.co@gmail.com>
2024-11-06 20:37:34 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,19 +17,19 @@ export default function Home() {
     <main className="relative grid  grid-cols-1 slg:grid-cols-2 gap-12  px-4 py-12 sm:py-16 sm:px-8 md:px-10 slg:p-16 lg:p-24">
       <div className="col-span-full group w-full flx flex-col items-center justify-center space-y-2 sm:space-y-4 mb-4 text-center">
         <Link
-          href="https://github.com/codebucks27/AI-Powered-Twitter-Bio-Generator"
+          href="https://buymeacoffee.com/ansh_sx"
           target="_blank"
           className=""
         >
           <AnimatedGradientText className="px-6 py-2 rounded-full">
             <Star className="w-6 h-6 fill-yellow-300 text-yellow-400" />
             <hr className="mx-2 h-4 w-[1px] bg-gray-300" />
-            Star on Github
+            Buy me a coffee ☕
             <ChevronRight className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
           </AnimatedGradientText>
         </Link>
         <h1 className="font-extrabold text-4xl md:text-5xl slg:text-6xl lg:text-7xl text-center w-full lg:w-[90%] uppercase mx-auto pt-4">
-          CRAFT THE PERFECT TWITTER BIO IN SECONDS!
+          CRAFT THE PERFECT SOCIAL MEDIA BIO IN SECONDS!
         </h1>
         <p className=" text-sm sm:text-base  md:text-lg text-accent">
           Just answer a few questions, and we&apos;ll generate a bio that captures
